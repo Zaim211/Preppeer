e4a3b32c35348ac46b80f972da3c541fd1761648
abc <abc@example.com>
2024-08-05 20:56:27 +0100

    Pagination

--- clients/src/components/Hero.jsx
+++ clients/src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext, useRef } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import { UserContext } from "../UserContext";
