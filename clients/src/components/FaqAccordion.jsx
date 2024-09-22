47bf30016dcbe50685d3a84bafa6bb5e1214f20f
piratesclub10 <piratesclub10@gmail.com>
2024-09-22 19:28:58 +0100

    Update FaqAccordion.jsx

--- clients/src/components/FaqAccordion.jsx
+++ clients/src/components/FaqAccordion.jsx
@@ -4,7 +4,7 @@ function FaqAccordion({title,content}) {
   return (
     <Accordion type="single" collapsible>
                 <AccordionItem value="item-1">
-                <AccordionTrigger className='text-3xl font-thin no-underline'>{title}</AccordionTrigger>
+                <AccordionTrigger className='lg:text-3xl text-xl font-thin no-underline'>{title}</AccordionTrigger>
                 <AccordionContent className='text-xl font-thin'>
                 {content}
                 </AccordionContent>
